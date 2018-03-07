6df1453c4d089d8a904e4b284b041298618a225c
Corey Powell <mistdragon100@gmail.com>
2018-03-07 00:08:43 -0500

    Drop codeclimate-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ Encoding.default_internal = Encoding.default_external = 'UTF-8'
 require 'codeclimate-test-reporter'
 require 'simplecov'
 
-CodeClimate::TestReporter.start
 SimpleCov.start
 
 require 'moon-memoized/load'
